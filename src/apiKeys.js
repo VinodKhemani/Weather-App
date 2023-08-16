3568cd5dd94e8a57353554bcefb078a1197f676e
Vinod <vinodkhemani9252@gmail.com>
2023-08-17 05:28:51 +0530

    Commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "54869a96cc3a088249ec9c9d8ea12565",
+  key: "Enter Your Api Key",
   base: "https://api.openweathermap.org/data/2.5/",
 };
